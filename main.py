1fa0a47e68a18513bba324e9ce08adc8c242f790
Katya Weicht <katya.weicht@gmail.com>
2025-07-21 14:31:53 -0600

    Select employee number and last name columns from the databse

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 # STEP 1A
 # Import SQL Library and Pandas
 import sqlite3
-import pandas
+import pandas as pd
 
 # STEP 1B
 # Connect to the database
 conn = sqlite3.connect('data.sqlite')
 
-
+employee_data = pd.read_sql("""SELECT * FROM employees""", conn)
+print(employee_data)
 # STEP 2
 # Replace None with your code
-df_first_five = None
+df_first_five = pd.read_sql("""SELECT employeeNumber,lastName FROM employees""",conn)
+print(df_first_five)
 
 # STEP 3
 # Replace None with your code
-df_five_reverse = None
+df_five_reverse = pd.read_sql("""SELECT lastName, employeeNumber FROM employees""",conn)
 
 # STEP 4
 # Replace None with your code
