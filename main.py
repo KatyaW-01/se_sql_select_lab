322b008ac1265e948384528646836fc8e9fed33d
Katya Weicht <katya.weicht@gmail.com>
2025-07-21 15:11:05 -0600

    Remove comments

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-# STEP 1A
-# Import SQL Library and Pandas
 import sqlite3
 import pandas as pd
 
-# STEP 1B
-# Connect to the database
+#Connect to the database
 conn = sqlite3.connect('data.sqlite')
 
-#employeeData table
+#Employee Data table
 employee_data = pd.read_sql("""SELECT * FROM employees""", conn)
 
-# STEP 2
-# Replace None with your code
+#Queries
 df_first_five = pd.read_sql("""SELECT employeeNumber,lastName FROM employees""",conn)
 
-# STEP 3
-# Replace None with your code
 df_five_reverse = pd.read_sql("""SELECT lastName, employeeNumber FROM employees""",conn)
 
-# STEP 4
-# Replace None with your code
 df_alias = pd.read_sql("""SELECT lastName, employeeNumber AS ID FROM employees""", conn)
 
-# STEP 5
-# Replace None with your code
 df_executive = pd.read_sql("""
 SELECT jobTITLE,
     CASE
@@ -35,23 +25,16 @@ SELECT jobTITLE,
   FROM employees;
 """,conn)
 
-# STEP 6
-# Replace None with your code
 df_name_length = pd.read_sql("""SELECT length(lastName) AS name_length from employees""",conn)
 
-# STEP 7
-# Replace None with your code
 df_short_title = pd.read_sql("""SELECT substr(jobTitle, 1, 2) AS short_title from employees""",conn)
 
-#order details table
+#Order details table
 order_details = pd.read_sql("""SELECT * FROM orderDetails;""", conn) 
 print(order_details)
 
-# STEP 8
-# Replace None with your code
 sum_total_price = pd.read_sql("""SELECT CAST(round(priceEach * quantityOrdered) AS INTEGER) AS total_price from orderDetails""",conn).sum()
 
-
-# STEP 9
-# Replace None with your code
 df_day_month_year = None
+
+conn.close()
